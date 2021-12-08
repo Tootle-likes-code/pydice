dbeb1582bcff1766bba7c3221220960c28e4973e
Jason Kennelly <tootle_likes_code@hotmail.com>
2021-12-08 12:48:42 +0000

    Added Rollable abstract class

--- die.py
+++ die.py
@@ -1,11 +1,27 @@
 import random
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
 from roll_result import RollResult
 
 
 @dataclass
-class Die:
+class Rollable(ABC):
+    @abstractmethod
+    def roll(self) -> list[int]:
+        pass
+
+    @property
+    def min(self) -> int:
+        pass
+
+    @property
+    def max(self) -> int:
+        pass
+
+
+@dataclass
+class Die(Rollable):
     sides: int
     minimum_roll: int = 1
 
