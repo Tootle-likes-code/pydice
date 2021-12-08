4f879ef305677324058e928404ee2a6b4334b5b7
Jason Kennelly <tootle_likes_code@hotmail.com>
2021-12-08 12:55:26 +0000

    Added DieRollResult and made RollResult abstract.

--- roll_result.py
+++ roll_result.py
@@ -1,10 +1,30 @@
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
 from die import Die
 
 
 @dataclass
-class RollResult:
+class RollResult(ABC):
+    @abstractmethod
+    @property
+    def die_rolls(self) -> list[int]:
+        pass
+
+    @abstractmethod
+    def result(self) -> int:
+        pass
+
+
+@dataclass
+class DieRollResult(RollResult):
     die: Die
     roll: int
 
+    @property
+    def die_rolls(self) -> list[int]:
+        die_roll_as_list = [self.roll]
+        return die_roll_as_list
+
+    def result(self) -> int:
+        return self.roll
