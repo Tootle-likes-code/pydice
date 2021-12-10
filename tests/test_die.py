0d596f8e337d618c44a7d66417930139d9c16ed8
Jason Kennelly <tootle_likes_code@hotmail.com>
2021-12-10 11:58:18 +0000

    Added tests to confirm that max is doing what is expected based on individual rolls.

--- tests/test_die.py
+++ tests/test_die.py
@@ -19,6 +19,17 @@ class MinTests(DieTests):
 
         # Assert
         self.assertEqual(expected_result, result)
+
+    def test_negative_min_returns_negative_min_value(self):
+        # Arrange
+        expected_result = -10
+        test_die = Die(6, -10)
+
+        # Act
+        result = test_die.min
+
+        # Assert
+        self.assertEqual(expected_result, result)
         
 
 class MaxTests(DieTests):
@@ -30,6 +41,28 @@ class MaxTests(DieTests):
         # Act
         result = test_die.max
         
+        # Assert
+        self.assertEqual(expected_result, result)
+
+    def test_max_is_incremental_of_negative_min(self):
+        # Arrange
+        expected_result = 3
+        test_die = Die(6, -2)
+
+        # Act
+        result = test_die.max
+
+        # Assert
+        self.assertEqual(expected_result, result)
+
+    def test_max_is_incremental_of_min_greater_than_zero(self):
+        # Arrange
+        expected_result = 7
+        test_die = Die(6, 2)
+
+        # Act
+        result = test_die.max
+
         # Assert
         self.assertEqual(expected_result, result)
 
