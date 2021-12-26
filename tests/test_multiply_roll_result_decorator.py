76933c19f2a7270a0ce34245f54e721a9ff5bdb0
Jason Kennelly <tootle_likes_code@hotmail.com>
2021-12-26 21:50:52 +0000

    Added tests to make sure that die_rolls is returned correctly from the parent.

--- tests/test_multiply_roll_result_decorator.py
+++ tests/test_multiply_roll_result_decorator.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import Mock
+from unittest.mock import Mock, PropertyMock
 
 from roll_result import MultiplyRollResultDecorator
 
@@ -8,16 +8,30 @@ class MultiplyRollResultDecoratorTests(unittest.TestCase):
     def setUp(self) -> None:
         self.test_die = Mock()
         self.test_die.result = Mock(return_value=5)
+        self.test_die.die_rolls = PropertyMock(return_value=[4, 5])
+
+        self.test_roll_result = MultiplyRollResultDecorator(self.test_die, 2)
+
+
+class DieRollsTests(MultiplyRollResultDecoratorTests):
+    def test_rolled_values_are_returned(self):
+        # Arrange
+        expected_result = [4, 5]
+
+        # Act
+        result = self.test_roll_result.die_rolls()
+
+        # Assert
+        self.assertEqual(expected_result, result)
 
 
 class ResultTests(MultiplyRollResultDecoratorTests):
     def test_result_is_multiplied_correctly(self):
         # Arrange
         expected_result = 10
-        test_roll_result = MultiplyRollResultDecorator(self.test_die, 2)
 
         # Act
-        result = test_roll_result.result()
+        result = self.test_roll_result.result()
 
         # Assert
         self.assertEqual(expected_result, result)
