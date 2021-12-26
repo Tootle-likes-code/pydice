d125c125f160d71d181f5dd7c5841fb8889287e7
Jason Kennelly <tootle_likes_code@hotmail.com>
2021-12-26 20:26:34 +0000

    Added tests to ensure that rounding is being done on the result.

--- tests/test_divide_by_roll_result_decorator.py
+++ tests/test_divide_by_roll_result_decorator.py
@@ -22,6 +22,28 @@ class ResultsTests(DivideByRollResultDecoratorTests):
         # Assert
         self.assertEqual(expected_result, result)
 
+    def test_remainder_result_is_rounded_up_correctly(self):
+        # Arrange
+        expected_result = 1
+        test_roll_result = DivideByRollResultDecorator(self.test_die, 5)
+
+        # Act
+        result = test_roll_result.result()
+
+        # Assert
+        self.assertEqual(expected_result, result)
+
+    def test_remainder_result_is_rounded_down_correctly(self):
+        # Arrange
+        expected_result = 1
+        test_roll_result = DivideByRollResultDecorator(self.test_die, 3)
+
+        # Act
+        result = test_roll_result.result()
+
+        # Assert
+        self.assertEqual(expected_result, result)
+
 
 if __name__ == '__main__':
     unittest.main()
