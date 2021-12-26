ecd7bc1835beedc51bc03322d8d5648a082e9030
Jason Kennelly <tootle_likes_code@hotmail.com>
2021-12-26 21:47:25 +0000

    Added tests to make sure that die_rolls is returned correctly from the parent.

--- tests/test_add_to_roll_result_decorator.py
+++ tests/test_add_to_roll_result_decorator.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import Mock
+from unittest.mock import Mock, PropertyMock
 
 from roll_result import AddToRollResultDecorator
 
@@ -8,16 +8,30 @@ class AddToRollResultDecoratorTests(unittest.TestCase):
     def setUp(self) -> None:
         self.test_die = Mock()
         self.test_die.result = Mock(return_value=3)
+        self.test_die.die_rolls = PropertyMock(return_value=[4, 5])
+
+        self.test_roll_result = AddToRollResultDecorator(self.test_die, 2)
+
+
+class DieRollsTests(AddToRollResultDecoratorTests):
+    def test_rolled_values_are_returned(self):
+        # Arrange
+        expected_result = [4, 5]
+
+        # Act
+        result = self.test_roll_result.die_rolls()
+
+        # Assert
+        self.assertEqual(expected_result, result)
 
 
 class ResultTests(AddToRollResultDecoratorTests):
     def test_results_has_modifier_added_to_it(self):
         # Arrange
         expected_result = 5
-        test_roll_result = AddToRollResultDecorator(self.test_die, 2)
 
         # Act
-        result = test_roll_result.result()
+        result = self.test_roll_result.result()
 
         # Assert
         self.assertEqual(expected_result, result)
