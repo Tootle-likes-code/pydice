f12b6abe1e16605d0210b8203a93d285884082d6
Jason Kennelly <tootle_likes_code@hotmail.com>
2022-01-06 14:32:18 +0000

    Added tests add_die_roll to prove invalid numbers cannot be added.

--- tests/test_dice_roll_results.py
+++ tests/test_dice_roll_results.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import MagicMock, Mock
+from unittest.mock import MagicMock
 
 from pydice.die import Dice
 from pydice.roll_result import DiceRollResult
@@ -8,6 +8,8 @@ from pydice.roll_result import DiceRollResult
 class DiceRollResultTests(unittest.TestCase):
     def setUp(self) -> None:
         self.mock_dice = MagicMock(spec=Dice)
+        self.mock_dice.min = 2
+        self.mock_dice.max = 12
 
         self.test_dice_roll_results = DiceRollResult(self.mock_dice, [4, 5])
 
@@ -61,26 +63,57 @@ class ResultsTest(DiceRollResultTests):
 class AddDieRollTests(DiceRollResultTests):
     def test_adding_roll_adds_to_result(self):
         # Arrange
-        expected_result = [4, 5, 9]
-        mock_roll_result = MagicMock()
-        mock_roll_result.result = Mock(return_value=9)
+        expected_result = [4, 5, 3]
 
         # Act
-        self.test_dice_roll_results.add_die_roll(mock_roll_result)
+        self.test_dice_roll_results.add_die_roll(3)
         result = self.test_dice_roll_results.rolls
 
         # Assert
         self.assertEqual(expected_result, result)
 
-
-class AddRollTests(DiceRollResultTests):
-    def test_adding_roll_adds_to_result(self):
+    def test_adding_roll_less_than_min_raises_Value_Error(self):
         # Arrange
-        expected_result = [4, 5, -5]
+        expected_args = ('Roll must be less than min and greater than max. Dice Min: 2, Dice Max: 12, '
+                         'value to add: -50',)
 
         # Act
-        self.test_dice_roll_results.add_roll(-5)
-        result = self.test_dice_roll_results.rolls
+        with self.assertRaises(ValueError) as ex:
+            self.test_dice_roll_results.add_die_roll(-50)
+
+        # Assert
+        self.assertEqual(expected_args, ex.exception.args)
+
+    def test_adding_roll_more_than_max_raises_Value_Error(self):
+        # Arrange
+        expected_args = ('Roll must be less than min and greater than max. Dice Min: 2, Dice Max: 12, '
+                         'value to add: 50',)
+
+        # Act
+        with self.assertRaises(ValueError) as ex:
+            self.test_dice_roll_results.add_die_roll(50)
+
+        # Assert
+        self.assertEqual(expected_args, ex.exception.args)
+
+    def test_adding_roll_equal_to_min_adds_correctly(self):
+        # Arrange
+        expected_result = [4, 5, 2]
+
+        # Act
+        self.test_dice_roll_results.add_die_roll(2)
+        result = self.test_dice_roll_results.die_rolls
+
+        # Assert
+        self.assertEqual(expected_result, result)
+
+    def test_adding_roll_equal_to_max_adds_correctly(self):
+        # Arrange
+        expected_result = [4, 5, 12]
+
+        # Act
+        self.test_dice_roll_results.add_die_roll(12)
+        result = self.test_dice_roll_results.die_rolls
 
         # Assert
         self.assertEqual(expected_result, result)
