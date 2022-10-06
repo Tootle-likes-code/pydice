42166f70daaf3c01d0e91ab3ce3abd62a61a4468
Jason Kennelly <tootle_likes_code@hotmail.com>
2022-10-06 11:58:09 +0100

    pydice-4 Added FATE dice to the builder.

--- pydice/dice_result_builder.py
+++ pydice/dice_result_builder.py
@@ -1,10 +1,12 @@
-from pydice.die import Dice
+from abc import ABC, abstractmethod
+
+from pydice.die import Dice, FateDie
 from pydice.roll_result import RollResult, DiceRollResult, AddToRollResultDecorator
 
 
 class DiceResultBuilder:
     def __init__(self, dice: Dice):
-        self._dice = DiceRollResult(dice)
+        self._dice_result = DiceRollResult(dice)
 
     @staticmethod
     def create_dice_result_builder(dice):
@@ -12,8 +14,26 @@ class DiceResultBuilder:
         return dice_result_builder
 
     def with_add_modifier(self, modifier):
-        self._dice = AddToRollResultDecorator(self._dice, modifier)
+        self._dice_result = AddToRollResultDecorator(self._dice_result, int(modifier))
         return self
 
     def build(self) -> RollResult:
-        return self._dice
+        return self._dice_result
+
+
+class FateDiceResultBuilder:
+    fate_dice = Dice(FateDie(), 4)
+
+    def __init__(self):
+        self._dice_result = DiceRollResult(self.fate_dice)
+
+    @staticmethod
+    def create_fate_dice_result_builder():
+        fate_dice_result_builder = FateDiceResultBuilder()
+        return fate_dice_result_builder
+
+    def with_add_modifier(self, modifier):
+        self._dice_result = AddToRollResultDecorator(self._dice_result, int(modifier))
+
+    def build(self) -> RollResult:
+        return self._dice_result
