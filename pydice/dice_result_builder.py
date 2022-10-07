c48d4d5beb86dafb738bfcd4015ee15768adcc1a
Jason Kennelly <tootle_likes_code@hotmail.com>
2022-10-07 16:47:48 +0100

    pydice-4 Removed unnecessary FateDiceBuilder.

--- pydice/dice_result_builder.py
+++ pydice/dice_result_builder.py
@@ -41,21 +41,3 @@ class DiceResultBuilder:
 
     def build(self) -> RollResult:
         return self._dice_result
-
-
-class FateDiceResultBuilder:
-    fate_dice = Dice(FateDie(), 4)
-
-    def __init__(self):
-        self._dice_result = DiceRollResult(self.fate_dice)
-
-    @staticmethod
-    def create_fate_dice_result_builder():
-        fate_dice_result_builder = FateDiceResultBuilder()
-        return fate_dice_result_builder
-
-    def with_add_modifier(self, modifier):
-        self._dice_result = AddToRollResultDecorator(self._dice_result, int(modifier))
-
-    def build(self) -> RollResult:
-        return self._dice_result
