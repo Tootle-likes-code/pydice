69f1eecd65bc4078a9f98aaa379616b084227bff
Jason Kennelly <tootle_likes_code@hotmail.com>
2022-10-17 17:18:14 +0100

    refactor-dice-string-parser-create - Updated to implement ParsedDiceString.

--- tests/dice_strings_tests/test_string_interpreter_integration_tests.py
+++ tests/dice_strings_tests/test_string_interpreter_integration_tests.py
@@ -3,6 +3,7 @@ from unittest import skip
 from unittest.mock import patch
 
 from pydice.dice_string.dice_string_interpreter import interpret
+from pydice.dice_string.parsed_dice_string import ParsedDiceString
 from pydice.die import Dice, Die, FateDie
 from pydice.roll_result import DiceRollResult
 from pydice.roll_result_operators.counter_roll_result_decorator import CountValuesEqualToDecorator, \
@@ -30,10 +31,11 @@ class InterpretTests(StringInterpreterTests):
     def setUp(self) -> None:
         self._default_roll_result = dice_results[:1]
         self.d20 = Dice(Die(20), 1)
+        self.ten_d10 = Dice(Die(10), 10)
 
     def test_interpret_with_simple_roll_returns_correct_value(self, _):
         # Arrange
-        expected_result = DiceRollResult(self.d20, self._default_roll_result)
+        expected_result = ParsedDiceString("1d20", [], DiceRollResult(self.d20, [9]))
 
         # Act
         result = interpret("1d20")
@@ -43,7 +45,7 @@ class InterpretTests(StringInterpreterTests):
 
     def test_interpret_with_numberless_d20_returns_correct_dice(self, _):
         # Arrange
-        expected_result = DiceRollResult(self.d20, self._default_roll_result)
+        expected_result = ParsedDiceString("d20", [], DiceRollResult(self.d20, [9]))
 
         # Act
         result = interpret("d20")
@@ -53,7 +55,9 @@ class InterpretTests(StringInterpreterTests):
 
     def test_interpret_with_ten_d10_returns_correct_dice(self, _):
         # Arrange
-        expected_result = DiceRollResult(Dice(number_of_dice=10, die=Die(10)), dice_results)
+        expected_result = ParsedDiceString(
+            "10d10", [], DiceRollResult(self.ten_d10, [9, 10, 6, 7, 6, 1, 2, 4, 8, 3])
+        )
 
         # Act
         result = interpret("10d10")
@@ -63,7 +67,7 @@ class InterpretTests(StringInterpreterTests):
 
     def test_interpret_d_case_does_not_matter(self, _):
         # Arrange
-        expected_result = DiceRollResult(self.d20, self._default_roll_result)
+        expected_result = ParsedDiceString("1D20", [], DiceRollResult(self.d20, [9]))
 
         # Act
         result = interpret("1D20")
@@ -73,7 +77,11 @@ class InterpretTests(StringInterpreterTests):
 
     def test_interpret_add_adds_decorator(self, _):
         # Arrange
-        expected_result = AddToRollResultDecorator(DiceRollResult(self.d20, self._default_roll_result), 5)
+        expected_result = ParsedDiceString(
+            "1d20+5",
+            [],
+            AddToRollResultDecorator(DiceRollResult(self.d20, [9]), 5)
+        )
 
         # Act
         result = interpret("1d20+5")
@@ -83,15 +91,12 @@ class InterpretTests(StringInterpreterTests):
 
     def test_interpret_adding_twice_adds_decorator_twice(self, _):
         # Arrange
-        expected_result = AddToRollResultDecorator(
+        expected_result = ParsedDiceString(
+            "1d20+5+6",
+            [],
             AddToRollResultDecorator(
-                DiceRollResult(
-                    self.d20,
-                    self._default_roll_result
-                ),
-                5
-            ),
-            6
+                AddToRollResultDecorator(DiceRollResult(self.d20, [9]), 5),
+                6)
         )
 
         # Act
@@ -102,7 +107,11 @@ class InterpretTests(StringInterpreterTests):
 
     def test_interpret_subtract_adds_decorator(self, _):
         # Arrange
-        expected_result = SubtractFromRollResultDecorator(DiceRollResult(self.d20, self._default_roll_result), 5)
+        expected_result = ParsedDiceString(
+            "1d20-5",
+            [],
+            SubtractFromRollResultDecorator(DiceRollResult(self.d20, [9]), 5)
+        )
 
         # Act
         result = interpret("1d20-5")
@@ -112,7 +121,11 @@ class InterpretTests(StringInterpreterTests):
 
     def test_interpret_multiply_adds_decorator(self, _):
         # Arrange
-        expected_result = MultiplyRollResultDecorator(DiceRollResult(self.d20, self._default_roll_result), 5)
+        expected_result = ParsedDiceString(
+            "1d20*5",
+            [],
+            MultiplyRollResultDecorator(DiceRollResult(self.d20, [9]), 5)
+        )
 
         # Act
         result = interpret("1d20*5")
@@ -122,7 +135,11 @@ class InterpretTests(StringInterpreterTests):
 
     def test_interpret_divide_adds_decorator(self, _):
         # Arrange
-        expected_result = DivideByRollResultDecorator(DiceRollResult(self.d20, self._default_roll_result), 5)
+        expected_result = ParsedDiceString(
+            "1d20/5",
+            [],
+            DivideByRollResultDecorator(DiceRollResult(self.d20, [9]), 5)
+        )
 
         # Act
         result = interpret("1d20/5")
@@ -132,7 +149,11 @@ class InterpretTests(StringInterpreterTests):
 
     def test_interpret_equal_to_adds_decorator(self, _):
         # Arrange
-        expected_result = CountValuesEqualToDecorator(DiceRollResult(self.d20, self._default_roll_result), 9)
+        expected_result = ParsedDiceString(
+            "1d20=9",
+            [],
+            CountValuesEqualToDecorator(DiceRollResult(self.d20, [9]), 9)
+        )
 
         # Act
         result = interpret("1d20=9")
@@ -142,7 +163,10 @@ class InterpretTests(StringInterpreterTests):
 
     def test_interpret_greater_than_adds_decorator(self, _):
         # Arrange
-        expected_result = CountValuesGreaterThanDecorator(DiceRollResult(self.d20, self._default_roll_result), 9)
+        expected_result = ParsedDiceString(
+            "1d20>9",
+            [],
+            CountValuesGreaterThanDecorator(DiceRollResult(self.d20, self._default_roll_result), 9))
 
         # Act
         result = interpret("1d20>9")
@@ -152,10 +176,14 @@ class InterpretTests(StringInterpreterTests):
 
     def test_interpret_greater_than_equal_to_adds_greater_than_and_equal_to_decorator(self, _):
         # Arrange
-        expected_result = CountValuesGreaterThanDecorator(
-            CountValuesEqualToDecorator(
-                DiceRollResult(self.d20, self._default_roll_result), 15
-            ), 15
+        expected_result = ParsedDiceString(
+            "1d20>=15",
+            [],
+            CountValuesGreaterThanDecorator(
+                CountValuesEqualToDecorator(
+                    DiceRollResult(self.d20, self._default_roll_result), 15
+                ), 15
+            )
         )
 
         # Act
@@ -166,7 +194,11 @@ class InterpretTests(StringInterpreterTests):
 
     def test_interpret_less_than_adds_decorator(self, _):
         # Arrange
-        expected_result = CountValuesLessThanDecorator(DiceRollResult(self.d20, self._default_roll_result), 9)
+        expected_result = ParsedDiceString(
+            "1d20<9",
+            [],
+            CountValuesLessThanDecorator(DiceRollResult(self.d20, self._default_roll_result), 9)
+        )
 
         # Act
         result = interpret("1d20<9")
@@ -176,10 +208,14 @@ class InterpretTests(StringInterpreterTests):
 
     def test_interpret_less_than_equal_to_adds_decorator(self, _):
         # Arrange
-        expected_result = CountValuesLessThanDecorator(
-            CountValuesEqualToDecorator(
-                DiceRollResult(self.d20, self._default_roll_result), 9
-            ), 9
+        expected_result = ParsedDiceString(
+            "1d20<=9",
+            [],
+            CountValuesLessThanDecorator(
+                CountValuesEqualToDecorator(
+                    DiceRollResult(self.d20, self._default_roll_result), 9
+                ), 9
+            )
         )
 
         # Act
@@ -190,7 +226,11 @@ class InterpretTests(StringInterpreterTests):
 
     def test_interpret_not_equal_to_adds_decorator(self, _):
         # Arrange
-        expected_result = CountValuesNotEqualToDecorator(DiceRollResult(self.d20, self._default_roll_result), 9)
+        expected_result = ParsedDiceString(
+            "1d20!=9",
+            [],
+            CountValuesNotEqualToDecorator(DiceRollResult(self.d20, self._default_roll_result), 9)
+        )
 
         # Act
         result = interpret("1d20!=9")
@@ -200,7 +240,11 @@ class InterpretTests(StringInterpreterTests):
 
     def test_interpret_not_equal_to_alt_adds_decorator(self, _):
         # Arrange
-        expected_result = CountValuesNotEqualToDecorator(DiceRollResult(self.d20, self._default_roll_result), 9)
+        expected_result = ParsedDiceString(
+            "1d20=/=9",
+            [],
+            CountValuesNotEqualToDecorator(DiceRollResult(self.d20, self._default_roll_result), 9)
+        )
 
         # Act
         result = interpret("1d20=/=9")
@@ -211,9 +255,12 @@ class InterpretTests(StringInterpreterTests):
     def test_interpret_explodes_adds_decorator(self, mock_die_rolls):
         # Arrange
         mock_die_rolls.side_effect = [4] + default_story_teller_dice_results
-        expected_result = ExplodeDiceForTargetDecorator(DiceRollResult(Dice(Die(10), 14),
-                                                                       default_story_teller_dice_results[:-1]), 10)
-
+        expected_result = ParsedDiceString(
+            "14d10e10",
+            [],
+            ExplodeDiceForTargetDecorator(DiceRollResult(Dice(Die(10), 14),
+                                                         default_story_teller_dice_results[:-1]), 10)
+        )
 
         # Act
         result = interpret("14d10e10")
@@ -241,7 +288,7 @@ class InterpretWithFateTests(StringInterpreterTests):
 
     def test_interpret_fate_die_rolls_four_dice(self, _):
         # Arrange
-        expected_result = DiceRollResult(self.fate_dice, default_fate_result)
+        expected_result = ParsedDiceString("df", [], DiceRollResult(self.fate_dice, default_fate_result))
 
         # Act
         result = interpret("df")
@@ -251,7 +298,11 @@ class InterpretWithFateTests(StringInterpreterTests):
 
     def test_interpret_fate_die_with_add_is_added(self, _):
         # Arrange
-        expected_result = AddToRollResultDecorator(DiceRollResult(self.fate_dice, default_fate_result), 2)
+        expected_result = ParsedDiceString(
+            "df+2",
+            [],
+            AddToRollResultDecorator(DiceRollResult(self.fate_dice, default_fate_result), 2)
+        )
 
         # Act
         result = interpret("df+2")
@@ -261,7 +312,7 @@ class InterpretWithFateTests(StringInterpreterTests):
 
     def test_interpret_fate_die_case_insensitive(self, _):
         # Arrange
-        expected_result = DiceRollResult(self.fate_dice, default_fate_result)
+        expected_result = ParsedDiceString("DF", [], DiceRollResult(self.fate_dice, default_fate_result))
 
         # Act
         result = interpret("DF")
@@ -277,12 +328,17 @@ class StorytellerInterpretTest(StringInterpreterTests):
 
     def test_storyteller_dice_are_correctly_interpreted(self, _):
         # Arrange
-        expected_result = CountValuesGreaterThanDecorator(
-            CountValuesEqualToDecorator(
+        expected_result = ParsedDiceString(
+            "15st",
+            [],
+            CountValuesGreaterThanDecorator(
                 CountValuesEqualToDecorator(
-                    DiceRollResult(self._test_dice, default_story_teller_dice_results), 10),
-                7),
-            7)
+                    CountValuesEqualToDecorator(
+                        DiceRollResult(self._test_dice, default_story_teller_dice_results), 10),
+                    7),
+                7
+            )
+        )
 
         # Act
         result = interpret("15st")
@@ -292,14 +348,19 @@ class StorytellerInterpretTest(StringInterpreterTests):
 
     def test_storyteller_dice_adding_successes_correctly_interpreted(self, _):
         # Arrange
-        expected_result = AddToRollResultDecorator(
-            CountValuesGreaterThanDecorator(
-                CountValuesEqualToDecorator(
+        expected_result = ParsedDiceString(
+            "15st+7",
+            [],
+            AddToRollResultDecorator(
+                CountValuesGreaterThanDecorator(
                     CountValuesEqualToDecorator(
-                        DiceRollResult(self._test_dice, default_story_teller_dice_results), 10),
+                        CountValuesEqualToDecorator(
+                            DiceRollResult(self._test_dice, default_story_teller_dice_results), 10),
+                        7),
                     7),
-                7),
-            7)
+                7
+            )
+        )
 
         # Act
         result = interpret("15st+7")
@@ -309,12 +370,17 @@ class StorytellerInterpretTest(StringInterpreterTests):
 
     def test_storyteller_case_insensitive(self, _):
         # Arrange
-        expected_result = CountValuesGreaterThanDecorator(
-            CountValuesEqualToDecorator(
+        expected_result = ParsedDiceString(
+            "15ST",
+            [],
+            CountValuesGreaterThanDecorator(
                 CountValuesEqualToDecorator(
-                    DiceRollResult(self._test_dice, default_story_teller_dice_results), 10),
-                7),
-            7)
+                    CountValuesEqualToDecorator(
+                        DiceRollResult(self._test_dice, default_story_teller_dice_results), 10),
+                    7),
+                7
+            )
+        )
 
         # Act
         result = interpret("15ST")
