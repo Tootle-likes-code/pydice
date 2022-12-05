8535a7f0e7eec6ee42aab79b5883bad0124a2b8c
Jason Kennelly <tootle_likes_code@hotmail.com>
2022-12-05 13:09:45 +0000

    refactor-dice-string-parser-create - Changed factory to be an object.

--- pydice/dice_string/dice_string_parser.py
+++ pydice/dice_string/dice_string_parser.py
@@ -1,6 +1,89 @@
-from pydice.dice_string.dice_string_parser_factory import DiceStringParserFactory
+import re
+from re import Pattern, Match
+
+from pydice.dice_string.operators import OperatorFactory
 from pydice.dice_string.parsed_dice_string import ParsedDiceString
+from pydice.dice_string.parsed_dice_string_builder import ParsedDiceStringBuilder
+from pydice.die import Dice, Die
 
 
 def parse(dice_string: str) -> ParsedDiceString:
-    return DiceStringParserFactory.create_parsed_dice_string(dice_string)
+    dice_string_parser = DiceStringParser(dice_string)
+    dice_string_parser.parse()
+
+    return dice_string_parser.parsed_dice_string
+
+
+_fate_regex = re.compile(r"df", re.IGNORECASE)
+_storyteller_regex = re.compile(r"(?P<number_of_dice>\d+)st", re.IGNORECASE)
+_base_dice_regex = re.compile(r"(?:\d*d\d.)|(?:\d+st)|(?:df)", re.IGNORECASE)
+_extract_dice_regex = re.compile(r"(?P<number_of_dice>\d*)d(?P<dice_size>\d+)", re.IGNORECASE)
+
+
+class DiceStringParser:
+    def __init__(self, dice_string):
+        self._parsed_dice_string = None
+        self._dice_string = dice_string
+        self._builder = ParsedDiceStringBuilder.create_parsed_dice_string(dice_string)
+
+    def parse(self) -> None:
+        self._extract_dice()
+        self._extract_operators()
+        self._parsed_dice_string = self._builder.build()
+
+    def _extract_dice(self):
+        extracted_special_dice = self._extract_reserved_dice()
+        if extracted_special_dice:
+            return
+
+        self._extract_generic_dice()
+
+    def _extract_reserved_dice(self) -> bool:
+        if self._check_string_for_dice(_fate_regex):
+            self._builder.with_fate_dice()
+            return True
+
+        storyteller_match = self._check_string_for_dice(_storyteller_regex)
+        if storyteller_match:
+            self._create_storyteller_dice(storyteller_match)
+            return True
+
+        return False
+
+    def _check_string_for_dice(self, dice_regex: Pattern) -> Match:
+        match = re.match(dice_regex, self._dice_string)
+
+        return match
+
+    def _create_storyteller_dice(self, storyteller_match: Match) -> None:
+        number_of_dice = int(storyteller_match.group("number_of_dice"))
+        self._builder.with_storyteller_dice(number_of_dice)
+        self._builder.with_operators(OperatorFactory.get_storyteller_operators())
+
+    def _extract_generic_dice(self):
+        dice_match = re.match(_extract_dice_regex, self._dice_string)
+        if not dice_match:
+            self._builder.with_dice_failure()
+            return
+
+        number_of_dice = int(dice_match.group("number_of_dice")) if \
+            "number_of_dice" in dice_match.groupdict().keys() \
+            and dice_match.group("number_of_dice") != '' \
+            else 1
+        dice_size = int(dice_match.group("dice_size"))
+
+        self._builder.with_dice(Dice(Die(dice_size), number_of_dice))
+
+    def _extract_operators(self) -> None:
+        operator_string = self._get_operator_string()
+        if not operator_string:
+            return
+        self._builder.with_operators(OperatorFactory.get_operators(operator_string))
+
+    def _get_operator_string(self):
+        modifiers = re.split(_base_dice_regex, self._dice_string)
+        return modifiers[1] if len(modifiers) > 1 else None
+
+    @property
+    def parsed_dice_string(self) -> ParsedDiceString | None:
+        return self._parsed_dice_string
--- tests/dice_strings_tests/test_dice_string_parser.py
+++ tests/dice_strings_tests/test_dice_string_parser.py
@@ -1,27 +1,135 @@
 import unittest
-from unittest.mock import call, patch
+from unittest.mock import MagicMock, patch, call
 
-from pydice.dice_string import dice_string_parser
-from tests.dice_strings_tests.test_parsed_dice_string import ParsedDiceStringTests
+from pydice.dice_string.dice_string_parser import DiceStringParser
+from pydice.dice_string.parsed_dice_string_builder import ParsedDiceStringBuilder
+from pydice.dice_string.operators import AddOperator, OperatorFactory
+from pydice.die import Dice, Die
+from tests import helpers
 
 
 class DiceStringParserTests(unittest.TestCase):
     def setUp(self) -> None:
-        pass
+        self.mock_builder = MagicMock(spec=ParsedDiceStringBuilder)
+        self.mock_builder.build.return_value = "ParsedDiceString"
 
 
-@patch("pydice.dice_string.dice_string_parser.DiceStringParserFactory.create_parsed_dice_string")
-class ParseTests(ParsedDiceStringTests):
-    def test_calls_dice_string_parser_factory(self, mock_factory):
+@patch("pydice.dice_string.dice_string_parser.ParsedDiceStringBuilder.create_parsed_dice_string")
+class ParseTests(DiceStringParserTests):
+    def test_given_valid_dice_string_calls_builder_with_dice(self, mock_create_builder):
         # Arrange
-        expected_calls = [call("1d20")]
+        expected_calls = [call(Dice(Die(20), 1))]
+        mock_create_builder.return_value = self.mock_builder
 
         # Act
-        dice_string_parser.parse("1d20")
+        DiceStringParser("1d20").parse()
 
         # Assert
-        mock_factory.assert_called_once()
-        mock_factory.assert_has_calls(expected_calls)
+        self.mock_builder.with_dice.assert_called_once()
+        self.mock_builder.with_dice.assert_has_calls(expected_calls)
+
+    def test_invalid_dice_adds_calls_builder_failure(self, mock_create_builder):
+        # Arrange
+        mock_create_builder.return_value = self.mock_builder
+
+        # Act
+        DiceStringParser("1d").parse()
+
+        # Assert
+        self.mock_builder.with_dice_failure.assert_called_once()
+
+    def test_fate_dice_calls_builder_add_fate_dice(self, mock_create_builder):
+        # Arrange
+        mock_create_builder.return_value = self.mock_builder
+
+        # Act
+        DiceStringParser("df").parse()
+
+        # Assert
+        self.mock_builder.with_fate_dice.assert_called_once()
+
+    def test_storyteller_dice_calls_builder_add_storyteller_dice(self, mock_create_builder):
+        # Arrange
+        expected_calls = [call(5)]
+        mock_create_builder.return_value = self.mock_builder
+
+        # Act
+        DiceStringParser("5st").parse()
+
+        # Assert
+        self.mock_builder.with_storyteller_dice.assert_called_once()
+        self.mock_builder.with_storyteller_dice.assert_has_calls(expected_calls)
+
+    def test_valid_dice_with_operators_calls_builder_add_operator(self, mock_create_builder):
+        # Arrange
+        expected_calls = [call([AddOperator(4)])]
+        mock_create_builder.return_value = self.mock_builder
+
+        # Act
+        DiceStringParser("1d20+4").parse()
+
+        # Assert
+        self.mock_builder.with_operators.assert_called_once()
+        self.mock_builder.with_operators.assert_has_calls(expected_calls)
+
+    def test_no_modifiers_adds_no_modifiers(self, mock_create_builder):
+        # Arrange
+        mock_create_builder.return_value = self.mock_builder
+
+        # Act
+        DiceStringParser("1d20").parse()
+
+        # Assert
+        self.mock_builder.with_operators.assert_not_called()
+
+    def test_storyteller_dice_calls_builder_add_storyteller_operators(self, mock_create_builder):
+        # Arrange
+        expected_calls = [call(OperatorFactory.get_storyteller_operators())]
+        mock_create_builder.return_value = self.mock_builder
+
+        # Act
+        DiceStringParser("5st").parse()
+
+        # Assert
+        self.mock_builder.with_operators.assert_called_once()
+        self.mock_builder.with_operators.assert_has_calls(expected_calls)
+
+    def test_storyteller_dice_with_modifier_builder_adds_correct_operators(self, mock_create_builder):
+        # Arrange
+        expected_calls = [call(OperatorFactory.get_storyteller_operators()), call([AddOperator(4)])]
+        mock_create_builder.return_value = self.mock_builder
+
+        # Act
+        DiceStringParser("5st+4").parse()
+
+        # Assert
+        helpers.assert_is_calls(self.mock_builder.with_operators, expected_calls)
+
+
+@patch("pydice.dice_string.dice_string_parser.ParsedDiceStringBuilder.create_parsed_dice_string")
+class ParsedDiceStringTests(DiceStringParserTests):
+    def test_no_parse_called_returns_None(self, _):
+        # Arrange
+        dice_string_parser = DiceStringParser("1d20+4")
+
+        # Act
+        result = dice_string_parser.parsed_dice_string
+
+        # Assert
+        self.assertIsNone(result)
+
+    def test_valid_parse_sets_expected_ParsedDiceString(self, mock_create_builder):
+        # Arrange
+        expected_result = "ParsedDiceString"
+        mock_create_builder.return_value = self.mock_builder
+        dice_string_parser = DiceStringParser("1d20+4")
+
+        # Act
+        dice_string_parser.parse()
+        result = dice_string_parser.parsed_dice_string
+
+        # Assert
+        self.assertEqual(expected_result, result)
 
 
 if __name__ == '__main__':
