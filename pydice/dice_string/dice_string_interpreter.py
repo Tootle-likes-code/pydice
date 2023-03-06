aa91d1cbee54852c6b42113f6a4bcebd9bf1ee89
Jason Kennelly <tootle_likes_code@hotmail.com>
2023-03-06 11:20:30 +0000

    pydice-25 Added linting and type hinting.

--- pydice/dice_string/dice_string_interpreter.py
+++ pydice/dice_string/dice_string_interpreter.py
@@ -1,8 +1,22 @@
+"""
+Receives dices strings and then converts them into ParsedDiceStrings.
+
+Functions:
+==========
+interpret(dice_string: str) -> ParsedDiceString
+    Takes a Dice String and processes it into a ParsedDiceString.
+"""
+
 from pydice.dice_string import dice_string_parser
 from pydice.dice_string.parsed_dice_string import ParsedDiceString
 
 
-def interpret(dice_string) -> ParsedDiceString:
+def interpret(dice_string: str) -> ParsedDiceString:
+    """
+    Converts a dice string into a ParsedDiceString.
+    :param dice_string: A string representation of a Dice Pool and it's modifiers.
+    :return:
+    """
     parsed_dice_string = dice_string_parser.parse(dice_string)
 
     return parsed_dice_string
