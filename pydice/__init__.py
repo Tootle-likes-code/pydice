ed095271a9ab8d3ecf134b3d0025ab1a13926d27
Jason Kennelly <tootle_likes_code@hotmail.com>
2023-03-09 01:30:56 +0000

    Added imports for interpret and RollResultBuilder.

---
+++ pydice/__init__.py
@@ -0,0 +1,11 @@
+"""
+The base module for pydice.
+
+Imports:
+========
+
+pydice.dice_string.dice_string_interpreter.interpret
+pydice.roll_result_operators.roll_result_builder.RollResultBuilder
+"""
+from pydice.dice_string.dice_string_interpreter import interpret
+from pydice.roll_result_operators.roll_result_builder import RollResultBuilder
