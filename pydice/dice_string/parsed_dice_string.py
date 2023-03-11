3cb0e95c879e9fcf9c75fcc53280f3eb7ac87158
Jason Kennelly <tootle_likes_code@hotmail.com>
2023-03-11 05:48:58 +0000

    pydice-25: Added pylint for the module.

--- pydice/dice_string/parsed_dice_string.py
+++ pydice/dice_string/parsed_dice_string.py
@@ -1,3 +1,12 @@
+"""
+A module for containing dice strings that have been converted into
+a pydice format for use.
+
+Classes:
+========
+ParsedDiceString - A class that represents a dice string in pydice terms.
+"""
+
 from dataclasses import dataclass
 from typing import Optional
 
@@ -7,15 +16,37 @@ from pydice.roll_result import RollResult
 
 @dataclass
 class ParsedDiceString:
+    """
+    A representation of a Dice String whilst also creating a RollResult.
+
+    Attributes:
+    ===========
+    dice_string: str - The dice string that this ParsedDiceString represents.
+    roll_result: Optional[RollResult] - The generated RollResult, if a valid one could be created.
+
+    Properties:
+    ===========
+    is_valid -> bool - Returns if the ParsedDiceString was valid.
+    failures -> str - Returns the failures from parsing the Dice string as a comma seperated list.
+    """
     dice_string: str
     _failures: list[DiceParserFailure]
     roll_result: Optional[RollResult]
 
     @property
     def is_valid(self) -> bool:
-        return Severity.ERROR not in [failure.severity for failure in self._failures] and self.roll_result is not None
+        """
+        Returns if the ParsedDiceString is valid.
+        :return:
+        """
+        return Severity.ERROR not in [failure.severity for failure in self._failures] \
+            and self.roll_result is not None
 
     @property
     def failures(self) -> str:
+        """
+        Returns the failures that occurred in the creation of this ParsedDiceString, with each error
+        being separated by a comma.
+        :return:
+        """
         return ','.join([failure.reason for failure in self._failures])
-
