e3a748ddc194645676def2edb0a25da0b80b5090
Jason Kennelly <tootle_likes_code@hotmail.com>
2023-03-21 18:14:07 +0000

    pydice-25: Added pylinting.

--- pydice/roll_result_operators/counter_roll_result_decorator.py
+++ pydice/roll_result_operators/counter_roll_result_decorator.py
@@ -1,3 +1,17 @@
+"""
+RollResultDecorators that count values from within the RollResult before.
+
+They specifically will reset the result of the roll to the count.
+
+Classes:
+========
+CounterRollResultDecorator      - An abstract class intended to facilitate counting chains.
+CountValuesEqualToDecorator     - Counts all values that are equal to a given value.
+CountValuesGreaterThanDecorator - Counts all values that are greater than a given value.
+CountValuesLessThanDecorator    - Counts all values that are less than a given value.
+CountValuesNotEqualToDecorator  - Counts all values that are not equal a particular value.
+"""
+
 from abc import ABC
 from dataclasses import dataclass
 
@@ -6,8 +20,18 @@ from pydice.roll_result_operators.roll_result_decorators import RollResultDecora
 
 class CounterRollResultDecorator(RollResultDecorator, ABC):
     """
-    A dummy class for RollResultDecorator that identifies the class as one that should continue the
-    count from the previous count, to facilitate counting chains.
+    A base class expanding on the RollResultDecorator, intending to be subclassed.
+
+    Attributes:
+    ===========
+    roll_result: RollResult - The RollResult to decorate.
+
+    Properties
+    ==========
+    die_rolls: list[int]    - A read-only property that retrieves the RollResult.die_rolls.
+    result: int             - A read-only property that returns result of the rolls.
+    rolled_die: Die         - A read-only property that returns the Die rolled
+                              in the RollResult.
     """
 
 
@@ -15,6 +39,18 @@ class CounterRollResultDecorator(RollResultDecorator, ABC):
 class CountValuesEqualToDecorator(CounterRollResultDecorator):
     """
     A decorator that counts all roll results of n.
+
+    Attributes:
+    ===========
+    roll_result: RollResult - The RollResult to decorate.
+    target_number: int      - The number that rolls have to be equal to, in order to be counted.
+
+    Properties
+    ==========
+    die_rolls: list[int]    - A read-only property that retrieves the RollResult.die_rolls.
+    result: int             - A read-only property that returns result of the rolls.
+    rolled_die: Die         - A read-only property that returns the Die rolled
+                              in the RollResult.
     """
     target_number: int
 
@@ -28,6 +64,18 @@ class CountValuesEqualToDecorator(CounterRollResultDecorator):
 class CountValuesGreaterThanDecorator(CounterRollResultDecorator):
     """
     A decorator that counts all roll results greater than n.
+
+    Attributes:
+    ===========
+    roll_result: RollResult - The RollResult to decorate.
+    target_number: int      - The number that rolls have to be greater than, in order to be counted.
+
+    Properties
+    ==========
+    die_rolls: list[int]    - A read-only property that retrieves the RollResult.die_rolls.
+    result: int             - A read-only property that returns result of the rolls.
+    rolled_die: Die         - A read-only property that returns the Die rolled
+                              in the RollResult.
     """
     target_number: int
 
@@ -40,7 +88,19 @@ class CountValuesGreaterThanDecorator(CounterRollResultDecorator):
 @dataclass
 class CountValuesLessThanDecorator(CounterRollResultDecorator):
     """
-    A decorator that counts all roll results less than n.
+    A decorator that counts all roll results greater than n.
+
+    Attributes:
+    ===========
+    roll_result: RollResult - The RollResult to decorate.
+    target_number: int      - The number that rolls have to be less than, in order to be counted.
+
+    Properties
+    ==========
+    die_rolls: list[int]    - A read-only property that retrieves the RollResult.die_rolls.
+    result: int             - A read-only property that returns result of the rolls.
+    rolled_die: Die         - A read-only property that returns the Die rolled
+                              in the RollResult.
     """
     target_number: int
 
@@ -53,7 +113,19 @@ class CountValuesLessThanDecorator(CounterRollResultDecorator):
 @dataclass
 class CountValuesNotEqualToDecorator(CounterRollResultDecorator):
     """
-    A decorator that counts all roll results that do not equal n.
+    A decorator that counts all roll results not equal to n.
+
+    Attributes:
+    ===========
+    roll_result: RollResult - The RollResult to decorate.
+    target_number: int      - The number that rolls have to be not equal to, in order to be counted.
+
+    Properties
+    ==========
+    die_rolls: list[int]    - A read-only property that retrieves the RollResult.die_rolls.
+    result: int             - A read-only property that returns result of the rolls.
+    rolled_die: Die         - A read-only property that returns the Die rolled
+                              in the RollResult.
     """
     target_number: int
 
@@ -61,4 +133,3 @@ class CountValuesNotEqualToDecorator(CounterRollResultDecorator):
         count = sum(1 for roll in self.roll_result.die_rolls if roll != self.target_number)
         self._result = self.roll_result.result + count \
             if isinstance(self.roll_result, CounterRollResultDecorator) else count
-
