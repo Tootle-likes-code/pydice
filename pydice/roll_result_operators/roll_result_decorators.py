826de8109d3b399702014cb661cec6f18d3bf0aa
Jason Kennelly <tootle_likes_code@hotmail.com>
2023-03-21 18:02:33 +0000

    pydice-25: Added pylinting.

--- pydice/roll_result_operators/roll_result_decorators.py
+++ pydice/roll_result_operators/roll_result_decorators.py
@@ -1,6 +1,22 @@
+"""
+A collection of classes that can be used to decorate a RollResult class to transform
+and modify the results of that RollResult.
+
+
+Classes:
+========
+RollResultDecorator             - An abstract base class that defines the base decorator
+                                  functionality.
+AddToRollResultDecorator        - Adds a value to the RollResult.
+SubtractFromRollResultDecorator - Subtracts a value from the RollResult.
+MultipleRollResultDecorator     - Multiplies the RollResult by a value.
+DivideByRollResultDecorator     - Divides the RollResult by a value.
+ExplodeDiceForTargetDecorator   - When an individual die rolls the target number, it rolls
+                                  another die, adding the die to the RollResult.
+"""
+
 from abc import ABC
 from dataclasses import dataclass, field
-from typing import ClassVar
 
 from pydice.die import Die
 from pydice.roll_result import RollResult
@@ -9,28 +25,68 @@ from pydice.roll_result import RollResult
 @dataclass
 class RollResultDecorator(RollResult, ABC):
     """
-    A dummy class intended to be the base for decorator pattern.
+    A abstract base class for sub-classing for Decorator Pattern purposes, but offering default
+    properties for the subclasses to overwrite.
+
+    Attributes:
+    ===========
+    roll_result: RollResult - The RollResult to decorate.
+
+    Properties
+    ==========
+    die_rolls: list[int]    - A read-only property that retrieves the RollResult.die_rolls.
+    result: int             - A read-only property that returns result of the rolls.
+    rolled_die: Die         - A read-only property that returns the Die rolled
+                              in the RollResult.
     """
     roll_result: RollResult
     _result: int = field(init=False, repr=False, compare=False)
 
+    def __post_init__(self):
+        self._result = self.roll_result.result
+
     @property
     def die_rolls(self) -> list[int]:
+        """
+        Returns the sorted RollResult.die_rolls.
+        :return:
+        """
         return sorted(self.roll_result.die_rolls)
 
     @property
     def result(self) -> int:
+        """
+        Returns the result of the RollResult.
+        :return:
+        """
         return self._result
 
     @property
     def rolled_die(self) -> Die:
+        """
+        Returns the RollResult.rolled_die
+        :return:
+        """
         return self.roll_result.rolled_die
 
 
 @dataclass
 class AddToRollResultDecorator(RollResultDecorator):
     """
-    A decorator to add a modifier to the base result.
+    A decorator class that adds a number to the RollResult.
+
+    Attributes:
+    ===========
+    roll_result: RollResult - The RollResult to decorate.
+    modifier: int           - The number to be added to the RollResult.
+
+    Properties
+    ==========
+    die_rolls: list[int]    - A read-only property that retrieves the RollResult.die_rolls.
+    result: int             - A read-only property that returns result of the rolls,
+                              and the added modifier.
+    rolled_die: Die         - A read-only property that returns the Die rolled
+                              in the RollResult.
     """
     modifier: int
 
@@ -41,7 +97,20 @@ class AddToRollResultDecorator(RollResultDecorator):
 @dataclass
 class SubtractFromRollResultDecorator(RollResultDecorator):
     """
-    A decorator to subtract a modifier to the base result.
+    A decorator class that subtracts a number to the RollResult.
+
+    Attributes:
+    ===========
+    roll_result: RollResult - The RollResult to decorate.
+    modifier: int           - The number to be subtracted from the RollResult.
+
+    Properties
+    ==========
+    die_rolls: list[int]    - A read-only property that retrieves the RollResult.die_rolls.
+    result: int             - A read-only property that returns result of the rolls,
+                              and the added modifier.
+    rolled_die: Die         - A read-only property that returns the Die rolled
+                              in the RollResult.
     """
     modifier: int
 
@@ -52,7 +121,20 @@ class SubtractFromRollResultDecorator(RollResultDecorator):
 @dataclass
 class MultiplyRollResultDecorator(RollResultDecorator):
     """
-    A decorator to multiply the base result by a multiplier.
+    A decorator class that multiplies the RollResult by a number.
+
+    Attributes:
+    ===========
+    roll_result: RollResult - The RollResult to decorate.
+    multiplier: int         - The number to be multiply the RollResult by.
+
+    Properties
+    ==========
+    die_rolls: list[int]    - A read-only property that retrieves the RollResult.die_rolls.
+    result: int             - A read-only property that returns result of the rolls,
+                              and the added modifier.
+    rolled_die: Die         - A read-only property that returns the Die rolled
+                              in the RollResult.
     """
     multiplier: int
 
@@ -63,7 +145,20 @@ class MultiplyRollResultDecorator(RollResultDecorator):
 @dataclass
 class DivideByRollResultDecorator(RollResultDecorator):
     """
-    A decorator to divide the base result by a multiplier.
+    A decorator class that divides the RollResult by another number.
+
+    Attributes:
+    ===========
+    roll_result: RollResult - The RollResult to decorate.
+    divide_by: int          - The number to divide the RollResult by.
+
+    Properties
+    ==========
+    die_rolls: list[int]    - A read-only property that retrieves the RollResult.die_rolls.
+    result: int             - A read-only property that returns result of the rolls,
+                              and the added modifier.
+    rolled_die: Die         - A read-only property that returns the Die rolled
+                              in the RollResult.
     """
     divide_by: int
 
@@ -76,6 +171,19 @@ class ExplodeDiceForTargetDecorator(RollResultDecorator):
     """
     A decorator to cause rolls of n to be rolled again and added to the result.
     Values that have exploded are added where the exploded value appears.
+
+    Attributes:
+    ===========
+    roll_result: RollResult - The RollResult to decorate.
+    target_number: int      - The number that triggers the Die to reroll.
+
+    Properties
+    ==========
+    die_rolls: list[int]    - A read-only property that retrieves the RollResult.die_rolls.
+    result: int             - A read-only property that returns result of the rolls,
+                              and the added modifier.
+    rolled_die: Die         - A read-only property that returns the Die rolled
+                              in the RollResult.
     """
     target_number: int
 
@@ -100,50 +208,5 @@ class ExplodeDiceForTargetDecorator(RollResultDecorator):
 
     @property
     def die_rolls(self) -> list[int]:
+        """ Returns the die rolls and any values that may have exploded. """
         return self._new_die_rolls
-
-
-@dataclass
-class DropDiceDecorator(RollResultDecorator, ABC):
-    """
-    Root Class for all attempts to drop dice.
-    """
-    minimum_drop: ClassVar[int] = 1
-
-    @property
-    def result(self) -> int:
-        return sum(self.die_rolls)
-
-
-@dataclass
-class DropLowestDecorator(DropDiceDecorator):
-    """
-    A decorator that reduces the number of dice results by a specified number, keeping only
-    the highest values.
-    """
-    number_to_drop: int = 1
-
-    def __post_init__(self):
-        if self.number_to_drop < self.minimum_drop:
-            raise ValueError(f"Cannot drop less than {self.minimum_drop}.  Attempted to drop {self.number_to_drop}")
-
-    @property
-    def die_rolls(self) -> list[int]:
-        return sorted(self.roll_result.die_rolls)[self.number_to_drop:]
-
-@dataclass
-class DropHighestDecorator(DropDiceDecorator):
-    """
-    A decorator that reduces the number of die rolled by a specified number, keeping only the lowest values.
-    """
-    number_to_drop: int = 1
-    minimum_drop: ClassVar[int] = 1
-
-    def __post_init__(self):
-        if self.number_to_drop < self.minimum_drop:
-            raise ValueError(f"Cannot drop less than {self.minimum_drop}.  Attempted to drop {self.number_to_drop}")
-
-    @property
-    def die_rolls(self) -> list[int]:
-        dice_to_drop = self.number_to_drop * -1
-        return sorted(self.roll_result.die_rolls)[:dice_to_drop]
